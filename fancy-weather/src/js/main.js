07c3c44e0b93a0ada195ccb0bb3acfe1587a808c
Aliaksandr Senkevich <puskin1914@gmail.com>
2019-11-22 01:08:48 +0300

    feat: add loading background image

--- fancy-weather/src/js/main.js
+++ fancy-weather/src/js/main.js
@@ -1,6 +1,33 @@
 import { Skycons } from './skycons';
 
 
+let longitude;
+let latitude;
+const weatherTimezone = document.getElementById('weatherTimezone');
+
+const weatherTime = document.getElementById('weatherTime');
+const weatherTimeFirst = document.getElementById('weatherTimeFirst');
+const weatherTimeSecond = document.getElementById('weatherTimeSecond');
+const weatherTimeThird = document.getElementById('weatherTimeThird');
+
+const weatherDescreption = document.getElementById('weatherDescreption');
+const weatherDescreptionFirst = document.getElementById('weatherDescreptionFirst');
+const weatherDescreptionSecond = document.getElementById('weatherDescreptionSecond');
+const weatherDescreptionThird = document.getElementById('weatherDescreptionThird');
+
+const degreeValue = document.getElementById('degreesValue');
+const degreeValueFirst = document.getElementById('degreesValueFirst');
+const degreeValueSecond = document.getElementById('degreesValueSecond');
+const degreeValueThird = document.getElementById('degreesValueThird');
+
+const degreesSymbol = document.getElementById('degreesSymbol');
+const degreesSymbolFirst = document.getElementById('degreesSymbolFirst');
+const degreesSymbolSecond = document.getElementById('degreesSymbolSecond');
+const degreesSymbolThird = document.getElementById('degreesSymbolThird');
+
+const weatherToday = document.getElementById('weatherToday');
+
+
 // formatTime function
 function formatTime(t) {
   const timeNow = new Date(t * 1000);
@@ -17,31 +44,14 @@ function formatTime(t) {
 
 
 window.addEventListener('load', () => {
-  let longitude;
-  let latitude;
-  const weatherTimezone = document.getElementById('weatherTimezone');
-
-  const weatherTime = document.getElementById('weatherTime');
-  const weatherTimeFirst = document.getElementById('weatherTimeFirst');
-  const weatherTimeSecond = document.getElementById('weatherTimeSecond');
-  const weatherTimeThird = document.getElementById('weatherTimeThird');
-
-  const weatherDescreption = document.getElementById('weatherDescreption');
-  const weatherDescreptionFirst = document.getElementById('weatherDescreptionFirst');
-  const weatherDescreptionSecond = document.getElementById('weatherDescreptionSecond');
-  const weatherDescreptionThird = document.getElementById('weatherDescreptionThird');
-
-  const degreeValue = document.getElementById('degreesValue');
-  const degreeValueFirst = document.getElementById('degreesValueFirst');
-  const degreeValueSecond = document.getElementById('degreesValueSecond');
-  const degreeValueThird = document.getElementById('degreesValueThird');
-
-  const degreesSymbol = document.getElementById('degreesSymbol');
-  const degreesSymbolFirst = document.getElementById('degreesSymbolFirst');
-  const degreesSymbolSecond = document.getElementById('degreesSymbolSecond');
-  const degreesSymbolThird = document.getElementById('degreesSymbolThird');
-
-  const weatherToday = document.getElementById('weatherToday');
+  // !запрос на https://ipinfo.io/ бесполезен
+  //   const urlWithCoordinatesForIpinfo = 'https://ipinfo.io/json?token=a3d4d591bf436c';
+  //   // const urlWithCoordinates = 'https://ipinfo.io/json?token=eb5b90bb77d46a';
+  //   fetch(urlWithCoordinatesForIpinfo)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
 
 
   if (navigator.geolocation) {
@@ -145,3 +155,21 @@ window.addEventListener('load', () => {
     });
   }
 });
+
+
+async function getLinkToImage() {
+  // document.getElementById('inputText').value ||
+  // const informationInInput = 'Minsk';
+  // TODO or portrait
+  // const url = `https://api.unsplash.com/photos/random?query=town,${informationInInput}&client_id=97468db9ac3f46aba050edeb6ee94ee6c2fa732c4970c3d260cdbf50156a6f44`;
+  const url = 'https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=nature&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17';
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data);
+  const urlImg = data.urls.regular;
+  document.body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url('${urlImg}')`;
+  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundSize = '100vw 100vh';
+}
+
+document.getElementById('loadBackground').addEventListener('click', getLinkToImage);
